b12b3d284ccc34eb6d1656dd0889fd1890bb2f6d
Ashanti Haring <ashanti.haring@gmail.com>
2019-08-21 12:56:07 +0200

    scrollbar hover transition, padding top

--- src/App.js
+++ src/App.js
@@ -47,13 +47,13 @@ export default class App extends React.Component {
             path="/start"
             render={() => (
               <div>
-                {/*<Carousel/>*/}
+                <Carousel />
                 <Teaser
                   setPlayerUrl={this.setPlayerUrl}
                   genre={"full movie drama"}
                   category={"Drama"}
                 />
-                {/*<Teaser
+                <Teaser
                   setPlayerUrl={this.setPlayerUrl}
                   genre={"full movie comedy"}
                   category={"Comedy"}
@@ -72,7 +72,7 @@ export default class App extends React.Component {
                   setPlayerUrl={this.setPlayerUrl}
                   genre={"full movie family"}
                   category={"Family"}
-                />*/}
+                />
               </div>
             )}
           />
