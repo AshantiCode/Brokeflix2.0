ca8355083b9e318ef5d5f301d29f8d47a68b85bc
Ashanti Haring <ashanti.haring@gmail.com>
2020-05-24 14:40:24 +0200

    finish up new slider

--- src/Carousel.js
+++ src/Carousel.js
@@ -6,10 +6,22 @@ import "./App.css";
 export default class Carousel extends React.Component {
   componentDidMount() {
     this.swiper = new Swiper(".swiper-container", {
-      speed: 400,
+      effect: "coverflow",
+
+      centeredSlides: true,
+      speed: 700,
+      loop: true,
+      spaceBetween: 400,
       autoplay: {
-        delay: 5000,
+        delay: 4000,
         disableOnInteraction: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
     });
   }
@@ -19,18 +31,33 @@ export default class Carousel extends React.Component {
       <div className="swiper-container">
         <div className="swiper-wrapper">
           <div className="swiper-slide">
-            <img data-src="../public/assets/glow-hero2.jpg" alt="" />
+            <img
+              src="./assets/glow-hero2-copy.jpg"
+              className="slide"
+              alt="Glow"
+            />
           </div>
           <div className="swiper-slide">
-            <img data-src="../public/assets/kimmy-hero2.jpg" alt="" />
+            <img src="./assets/kimmy-hero2.jpg" className="slide" alt="Kimmy" />
           </div>
           <div className="swiper-slide">
-            <img data-src="../public/assets/trevor-hero.jpg" alt="" />
+            <img
+              src="./assets/trevor-hero.jpg"
+              className="slide"
+              alt="Trevor"
+            />
           </div>
           <div className="swiper-slide">
-            <img data-src="../public/assets/orange-hero.jpg" alt="" />
+            <img
+              src="./assets/orange-hero.jpg"
+              className="slide"
+              alt="Orange"
+            />
           </div>
         </div>
+        <div className="swiper-button-next"></div>
+        <div className="swiper-button-prev"></div>
+        <div className="swiper-pagination"></div>
       </div>
     );
   }
